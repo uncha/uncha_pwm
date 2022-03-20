b2a14b18ed6b37199ab6c3fb9928b4a5f7b06da0
kyutaekim <>
2022-03-20 20:38:35 +0900

    test

--- web.js
+++ web.js
@@ -36,6 +36,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.setHeader('Access-Control-Allow-Origin', 'http://44.202.126.254:3000');
+    res.setHeader('Access-Control-Allow-Origin', 'http://44.202.126.254:3001');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
